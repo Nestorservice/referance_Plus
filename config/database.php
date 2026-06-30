8cfc00212b12c640433534fd113926fafab01178
Nestorservice <163077025+Nestorservice@users.noreply.github.com>
2026-06-30 13:14:44 +0100

    fix(db): support MYSQL_ATTR_SSL_VERIFY_SERVER_CERT option for Aiven SSL connections

--- config/database.php
+++ config/database.php
@@ -54,7 +54,10 @@ return [
             'engine' => 'InnoDB',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', true),
+            ], function ($value) {
+                return $value !== null && $value !== '';
+            }) : [],
         ],
 
         'pgsql' => [
